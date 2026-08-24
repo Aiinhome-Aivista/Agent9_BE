c9d4dec3c816574a15939e38cc22263829c8aee9
Sathi Chakraborti <chakrabortisathi111@gmail.com>
2026-08-24 19:11:42 +0530

    deploy3

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "backend.main:app",
         host="0.0.0.0",
-        port=3004,
+        port=3035,
         reload=True,
         log_level="info"
     )
